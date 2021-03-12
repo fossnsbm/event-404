252a98f85f264157238ff741ab03c8cb23065670
Nimesh Kavinda <44240093+nimeshkavinda@users.noreply.github.com>
2021-03-13 00:01:28 +0530

    added vuetify

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import 'mdbvue/lib/css/mdb.min.css'
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
